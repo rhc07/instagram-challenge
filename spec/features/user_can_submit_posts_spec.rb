96ced72ee6bbd21630f647ad796e622b6c61f61d
rhc07 <rorieclarke@gmail.com>
2021-03-27 15:08:27 +0000

    added database

---
+++ spec/features/user_can_submit_posts_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# RSpec.feature "Posts page", type: :feature do 
+#     scenario "User can submit a post" do
+#         visit "/posts"
+#         click link "New post"
+#         fill_in "Message", with: "Hello rails!"
+#         click_button "Submit"
+#         expect(page).to have_content("Hello rails!")
+#     end
+# end
